3ac4aea0640a9e849c3619a6dbf0c7c914c3c84f
doanphuto <ahxd1996@gmail.com>
2016-12-14 13:45:23 +0700

    tao form HoSoBenhNhan

---
+++ BVM/BenhVienMat/BenhVienMat/HoSoBenhNhan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BenhVienMat
+{
+    public partial class HoSoBenhNhan : Form
+    {
+        public HoSoBenhNhan()
+        {
+            InitializeComponent();
+        }
+
+        private void HoSoBenhNhan_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
